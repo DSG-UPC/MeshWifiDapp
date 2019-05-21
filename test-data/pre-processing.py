cf9ff8681a1ccad98c17dd43f675c22a574a64e9
llunaCreixent <llunac@riseup.net>
2019-05-21 13:11:48 +0200

    db-QMPSU.json created with prepared data for the json-server.

--- test-data/pre-processing.py
+++ test-data/pre-processing.py
@@ -7,14 +7,12 @@ import os
 import sys
 from os import listdir
 import itertools
+import json
 
 if len(sys.argv) < 2:
-    print("---------- Welcome to this preprocessing script that transforms the dumps of the stations to a json file with amounts of data forwarded by nodes")
-    print("Usage:")
-    print("python3 pre-processing <raw-data-directory>")
-    print("the <raw-data-directory> is the directory where there are all the files *.iwdump.")
-    print("NOTE: <raw-data-directory> have to finish with '/'.")
-    print("----------")
+    with open("pre-processing-help-text", 'r') as fin:
+        print(fin.read())
+    fin.close()
     exit()
 
 dirName = sys.argv[1]
@@ -32,7 +30,6 @@ for fileName in listFiles:
         numOfFiles+=1
         nodeName = ""
         stationName = ""
-        filename = ""
         for line in f:
             if line.startswith("# start "):
                 nodeName = line.split()[4][1:-1]
@@ -41,18 +38,19 @@ for fileName in listFiles:
             elif line.startswith("Station "):
                 if (nodeName) not in dataset:
                     dataset[nodeName] = {}
-                dataset[nodeName][fileName] = {}
+                if (numOfFiles-1) not in dataset[nodeName]:
+                    dataset[nodeName][numOfFiles-1] = {}
                 stationName = line.split()[1]
                 #print(dataset[nodeName])
-                if (stationName) not in dataset[nodeName][fileName]:
-                    dataset[nodeName][fileName][stationName] = {}
+                if (stationName) not in dataset[nodeName][numOfFiles-1]:
+                    dataset[nodeName][numOfFiles-1][stationName] = {}
                 #print(line.split()[1])
                 continue
             elif line.startswith("	rx bytes:"):
-                dataset[nodeName][fileName][stationName]["rx"] =  line.split()[2]
+                dataset[nodeName][numOfFiles-1][stationName]["rx"] =  line.split()[2]
                 continue
             elif line.startswith("	tx bytes:"):
-                dataset[nodeName][fileName][stationName]["tx"] =  line.split()[2]
+                dataset[nodeName][numOfFiles-1][stationName]["tx"] =  line.split()[2]
 
 
 
@@ -70,7 +68,7 @@ dataset2 = {}
 lowLenCount = 0
 for node, timestamps in dataset.items():
     if len(timestamps) > 700:
-        print(len(timestamps))
+        #print(len(timestamps))
         dataset2[node] = timestamps
 
 
@@ -80,12 +78,48 @@ print("And we have this amount of files:",numOfFiles)
 
 
 jsonData = {}
-jsonData[monitor] = []
-jsonData[owner] = []
+jsonData["monitor"] = []
+jsonData["node"] = []
+
+# Reminder of the dataset structure:
+# dataset, nodeName, fileName, stationName, rx/tx
+
 
 for node, timestamps in dataset2.items():
-    pass
+    jsonData["node"].append({"id": node})
+    bytesT1 = 0
+    calculatedBytes = 0
+    #previousStationsValues = dataset[node][0]
+    calculatedBytesList = {}
+    for i in range(1,733):
+        calculatedBytes = 0
+        if (i-1) in dataset[node] and (i) in dataset[node]:
+            previousStationsValues = dataset[node][i-1]
+            nextStationsValues = dataset[node][i]
+            txBytes = 0
+            rxBytes = 0
+            #print(nextStationsValues)
+            for station in nextStationsValues:
+                #print(station)
+                if (station) in nextStationsValues and (station) in previousStationsValues:
+                    txBytes += int(nextStationsValues[station]["tx"]) - int(previousStationsValues[station]["tx"])
+                    rxBytes += int(nextStationsValues[station]["rx"]) - int(previousStationsValues[station]["rx"])
+                    #print(txBytes,rxBytes)
 
-jsonFile  = open("db-QMPSU.json", "w")
+            calculatedBytes += min(txBytes, rxBytes)
+            if calculatedBytes < 0:
+                calculatedBytes = 0
 
-jsonFile.close()
+        calculatedBytesList[i-1] = calculatedBytes
+
+    jsonData["monitor"].append({"id": node, "value": calculatedBytesList})
+
+
+
+#print(json.dumps(jsonData, indent=4))
+
+with open("db-QMPSU.json", "w") as outfile:
+    json.dump(jsonData, outfile, indent=4)
+outfile.close()
+
+print("Output file: db-QMPSU.json" )
