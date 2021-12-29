235d03f381c9aa23e86bc89fb5dcf61bc89d1024
emmdim <emmdim@users.noreply.github.com>
2021-12-29 17:30:36 +0100

    Add prometheus in docker-compose

--- truffle.js
+++ truffle.js
@@ -5,12 +5,12 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*",
-      from: '0x28b17326d08992f16d382db2923d0d8b4ff8adb0',
-    },
+    // development: {
+    //   host: "localhost",
+    //   port: 8545,
+    //   network_id: "*",
+    //   from: '0x28b17326d08992f16d382db2923d0d8b4ff8adb0',
+    // },
     staging: {
       provider: ganacheWeb3.currentProvider,
       network_id: "7775",
@@ -18,13 +18,13 @@ module.exports = {
       from: '0x28b17326d08992f16d382db2923d0d8b4ff8adb0',
       //from: "",
     },
-    meshdapp: {
-      provider: web3.currentProvider,
-      network_id: "7775",
-      gas: 8076844,
-      from: '0x28b17326d08992f16d382db2923d0d8b4ff8adb0',
-      //from: "",
-    },
+    // meshdapp: {
+    //   provider: web3.currentProvider,
+    //   network_id: "7775",
+    //   gas: 8076844,
+    //   from: '0x28b17326d08992f16d382db2923d0d8b4ff8adb0',
+    //   //from: "",
+    // },
   },
   compilers: {
     solc: {
