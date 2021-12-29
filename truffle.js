874c13b70ecf5dedd179dbaf3074ef69a331c649
emmdim <emmdim@users.noreply.github.com>
2021-12-29 18:29:41 +0100

    Update ganache container command

--- truffle.js
+++ truffle.js
@@ -5,17 +5,19 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
-    // development: {
-    //   host: "localhost",
-    //   port: 8545,
-    //   network_id: "*",
-    //   from: '0x28b17326d08992f16d382db2923d0d8b4ff8adb0',
-    // },
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",
+      from: '0xC79F7fE80B5676fe38D8187b79d55F7A61e702b2',
+    },
     staging: {
       provider: ganacheWeb3.currentProvider,
       network_id: "7775",
+      host: "localhost",
+      port: 8545,
       //gas: 5876844,
-      from: '0x28b17326d08992f16d382db2923d0d8b4ff8adb0',
+      from: '0xC79F7fE80B5676fe38D8187b79d55F7A61e702b2',
       //from: "",
     },
     // meshdapp: {
